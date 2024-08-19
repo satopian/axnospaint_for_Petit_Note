6a920ad9ecfef5b4977a89523797a5e83e592d4b
satopian <satopian@mia.eek.jp>
2024-08-19 10:24:24 +0900

    オーバーレイ等の合成時に白背景を含める。

--- src/js/window_layer.js
+++ src/js/window_layer.js
@@ -1242,8 +1242,15 @@ export class LayerSystem extends ToolWindow {
         let ctx = this.axpObj.CANVAS.main_ctx;
         //console.log('ここで描画', this.x_size, this.y_size);
         // 表示領域をクリア
-        this.CANVAS.backscreen_trans_ctx.beginPath();
+		this.CANVAS.backscreen_trans_ctx.beginPath();
         this.CANVAS.backscreen_trans_ctx.clearRect(0, 0, this.x_size, this.y_size);
+
+		if (!this.axpObj.assistToolSystem.getIsTransparent()){//背景透過ではない時に
+			//白背景を合成結果に含める
+			this.CANVAS.backscreen_trans_ctx.globalAlpha = 1;
+			this.CANVAS.backscreen_trans_ctx.fillStyle = '#ffffff';
+			this.CANVAS.backscreen_trans_ctx.fillRect(0, 0, this.x_size, this.y_size);
+		}
         // 全レイヤー走査（※下層レイヤーから描画するため逆順）
         // 処理済みindex（クリッピング処理で子を先行して合成する場合がある時、スキップ判定に使用する）
         let skipIdx = this.layerObj.length - 1;
